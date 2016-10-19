ae900d42752d0f0376a7c524f7db6333cb6c4f86
Aiman Farhan <rajaaimanf@gmail.com>
2016-10-20 06:29:06 +0800

    Added GA

--- html.js
+++ html.js
@@ -55,6 +55,15 @@ return (
       <TypographyStyle typography={typography} />
       <GoogleFont typography={typography} />
       {css}
+      <script dangerouslySetInnerHtml={{__html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-77344556-1', 'auto');
+ga('send', 'pageview');`}} />
+
+
       <script dangerouslySetInnerHTML={{ __html: `
     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
     n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
@@ -66,7 +75,6 @@ return (
     fbq('track', 'CompleteRegistration');
   `}}
 />
-
 
     </head>
     <body>
